7f7cc8e35ff54e7b42aac1cbd5308c4bcf53a5e0
ItzUltra <malteroslapat28@gmail.com>
2026-07-27 16:55:07 +0200

    ts2 added

--- script.js
+++ script.js
@@ -232,6 +232,95 @@ function closePopup() {
 
     popup.classList.add("hidden");
 
+},
+{
+    rank: 2,
+    name: "Thinking Space II",
+
+    creators: [
+        "Kugelblitz",
+        "Jawis",
+        "Hyperbola",
+        "CairoX",
+        "Realvet",
+        "UFWM",
+        "Rin04",
+        "maxmur",
+        "DiamondSkull",
+        "Meox",
+        "NineDice",
+        "RealNyanCat",
+        "GamerKnight750",
+        "ThunderDarkness",
+        "ErinaLaSacarina",
+        "Adrone",
+        "HaydenDom",
+        "cuber",
+        "kurewa",
+        "crowiley",
+        "Kevellium"
+    ],
+
+    verifier: "Zoink",
+
+    id: "119544028",
+
+    length: "1:10",
+
+    objects: "139593",
+
+    points: 1992,
+
+    minimumProgress: "65%",
+
+    enjoyment: "8/10",
+
+    skillset: [
+        "Timings",
+        "Spam"
+    ],
+
+    showcase:
+    "https://www.youtube.com/watch?v=fnz9ddZjIGc",
+
+    verification:
+    "https://www.youtube.com/watch?v=CELNmHwln_c",
+
+
+    records: [
+        {
+            player: "wPopOff",
+            percent: 100,
+            attempts: "Unknown",
+            video: "https://www.youtube.com/watch?v=hJHiGeuahTI",
+            type: "1st Victor"
+        },
+
+        {
+            player: "Trick",
+            percent: 100,
+            attempts: "Unknown",
+            video: "https://www.youtube.com/watch?v=HuUSRqbd6Ck",
+            type: "2nd Victor"
+        },
+
+        {
+            player: "NeterMind",
+            percent: 89,
+            attempts: "Unknown",
+            video: "https://youtube.com/watch?v=uqHsCNJxqmY"
+        },
+
+        {
+            player: "A6",
+            percent: 87,
+            attempts: "Unknown",
+            video: "https://www.youtube.com/watch?v=-ATaCVjkvT0"
+        }
+    ],
+
+
+    image: ""
 }
          
        
