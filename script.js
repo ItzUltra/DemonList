945366f4a75263d0dfb6fb6bc1c1f3c67a8029c2
ItzUltra <malteroslapat28@gmail.com>
2026-07-30 10:02:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -53,8 +53,72 @@ demons.forEach(demon => {
         </div>
     `;
 
+card.addEventListener("click", () => {
 
+    const popup = document.getElementById("popup");
+
+    popup.innerHTML = `
+        <div class="popup-content">
+
+            <h2>${demon.name}</h2>
+
+            <p><b>Position:</b> #${demon.rank}</p>
+
+            <p><b>Creators:</b></p>
+            <p>${demon.creators.join(", ")}</p>
+
+            <p><b>Level ID:</b> ${demon.id}</p>
+
+            <p><b>Length:</b> ${demon.length}</p>
+
+            <p><b>Object Count:</b> ${demon.objects}</p>
+
+            <p><b>Points:</b> ${demon.points}</p>
+
+            <p><b>Enjoyment:</b> ${demon.enjoyment}</p>
+
+            <p><b>Skillset:</b></p>
+
+            <div class="skills">
+                ${demon.skillset.map(skill => 
+                    `<span>${skill}</span>`
+                ).join("")}
+            </div>
+
+            <br>
+
+            <p>
+                <b>Showcase:</b>
+                <a href="${demon.showcase}" target="_blank">
+                    Watch
+                </a>
+            </p>
+
+            <p>
+                <b>Verification:</b>
+                <a href="${demon.verification}" target="_blank">
+                    Watch
+                </a>
+            </p>
+
+            <button onclick="closePopup()">
+                Close
+            </button>
+
+        </div>
+    `;
+
+    popup.classList.remove("hidden");
+
+});
+    
     mainList.appendChild(card);
 
 });
-      
+  unction closePopup() {
+
+    const popup = document.getElementById("popup");
+
+    popup.classList.add("hidden");
+
+}    
